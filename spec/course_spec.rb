ae2e7473ca264144d431d7c90836692743b6edb7
Justin Lee <justinslee201100@yahoo.com>
2023-09-06 10:02:25 -0600

    Add course test and code

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,50 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
+  describe '#Instance' do
+  it 'checks course is an instance of Course' do
+    course = Course.new("Calculus", 2)
+      
+    expect(course).to be_instance_of Course
+    end
+  end
+
+  describe '#Course start details' do
+    it 'has a name' do
+      course = Course.new("Calculus", 2)
+
+      expect(course.name).to eq("Calculus")
+    end
+
+    it 'has a capacity' do
+      course = Course.new("Calculus", 2)
+
+      expect(course.capacity).to eq(2)
+    end
+
+    it 'starts with no students' do
+      course = Course.new("Calculus", 2)
+
+      expect(course.students).to eq([])
+    end
+
+    it 'is not full' do
+      course = Course.new("Calculus", 2)
+
+      expect(course.full?).to be(false)
+    end
+  end
+
+  describe '#Course fill up' do
+    it 'fills the course up' do
+      course = Course.new("Calculus", 2)
+      student1 = Student.new({name: "Morgan", age: 21})
+      student2 = Student.new({name: "Jordan", age: 29})
+      course.enroll(student1)
+      course.enroll(student2)
+
+      expect(course.students).to eq([student1, student2])
+      expect(course.full?).to be(true)
+    end
+  end
 end
