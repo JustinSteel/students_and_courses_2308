eb08119eff2bb0f1525a288cded96e0708cfc616
Justin Lee <justinslee201100@yahoo.com>
2023-09-06 09:33:50 -0600

    Add Class detail and method to student

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
 class Student
+  attr_reader :name, :age, :scores
+
+  def initialize(student_atributes)
+    @name = student_atributes[:name]
+    @age = student_atributes[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    (@scores.sum.to_f / @scores.count.to_f)
+  end
   
 end
